b398ccef33ace2815b223abcc521e10273adfea2
Lucrecio Paes <lcpaes40@gmail.com>
2022-10-14 14:51:50 -0300

    criado projeto tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,8 @@
+public class TiposVariaveis {
+
+    public class App {
+        public static void main(String[] args) throws Exception {
+            System.out.println("Hello, World!");
+        }
+    }
+}
